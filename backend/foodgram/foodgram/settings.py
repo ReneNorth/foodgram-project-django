d29c2d49bb44522d980d4c5630102f58c666655a
Yury Zharikov <zharikovyuri@gmail.com>
2023-06-14 08:32:26 +0300

    nginx conf fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=os.path.abspath(
     os.path.join(BASE_DIR.parent, f'{os.pardir}/infra/.env')))
 SECRET_KEY = os.getenv('DJANGO_KEY')
-LOCAL_DEV = True
+LOCAL_DEV = False
 DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'web', '127.0.0.1', 'localhost', '127.0.0.1:8000']
@@ -135,6 +135,7 @@ if LOCAL_DEV is True:
         }
     }
 
+print(DATABASES)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
